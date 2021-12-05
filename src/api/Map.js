16de83adb677cb466b9e6d90e4e38df45932cb0f
hyunwk <ab8454@naver.com>
2021-12-05 19:18:23 +0900

    ✨ feat search cafe when mouse dragged

--- src/api/Map.js
+++ src/api/Map.js
@@ -8,14 +8,15 @@ const Map = () => {
   useEffect(() => {
     var container = document.getElementById('map');
     var options = {
-      center: new kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
-      level: 6,
+      center: new kakao.maps.LatLng(37.48955192726942, 126.91397340946133),
+      level: 3,
     };
     var map = new kakao.maps.Map(container, options);
     // 장소 검색 객체를 생성합니다
     var ps = new kakao.maps.services.Places(map);
     // 카테고리로 은행을 검색합니다
-    ps.categorySearch('CE7', placesSearchCB, { useMapBounds: true });
+    ps.categorySearch('CE7', placesSearchCB, { useMapCenter: true });
+    // ps.categorySearch('CE7', placesSearchCB, { useMapBounds: true });
 
     // 키워드 검색 완료 시 호출되는 콜백함수 입니다
     function placesSearchCB(data, status, pagination) {
@@ -45,6 +46,15 @@ const Map = () => {
         infowindow.open(map, marker);
       });
     }
+
+    // 마우스 드래그로 지도 이동이 완료되었을 때 마지막 파라미터로 넘어온 함수를 호출하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'dragend', function () {
+      // 지도 중심좌표를 얻어옵니다
+      var latlng = map.getCenter();
+
+      // 지도 이동이 완료되면 지도 중심을 기준으로, 카페를 재 검색합니다.
+      ps.categorySearch('CE7', placesSearchCB, { useMapCenter: true });
+    });
   }, []);
 
   return (
